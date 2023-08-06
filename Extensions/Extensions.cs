a1b551368cbab32416c120e5f32c485986a472e9
秉諺 李 <zxcv0129@gmail.com>
2023-08-06 15:18:29 +0800

    加入專案檔案。

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static int ToInt(this string value)
+        {
+            var result = int.TryParse(value, out int resultValue);
+            return result? resultValue : 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Extensions;
+
+var answer = 55;
+
+int intUserAnswer;
+int MinNumber = 0;
+int MaxNumber = 100;
+
+
+do
+{
+    Console.WriteLine($"請輸入介於{MinNumber}~{MaxNumber}之間的終極密碼");
+    
+    var userAnswer = Console.ReadLine();
+
+    intUserAnswer = userAnswer.ToInt();
+
+    
+
+        if (answer == intUserAnswer)
+    {
+        Console.WriteLine("答對了");
+    }
+
+    else if (intUserAnswer > MaxNumber || intUserAnswer < MinNumber)
+    {
+        Console.WriteLine("輸入的值超出範圍，請重新輸入");
+    }
+    else
+    {
+        if (intUserAnswer > answer)
+        {
+            Console.WriteLine($"答錯了，請輸入 {MinNumber} ~ {intUserAnswer} 之間的數值");
+
+            MaxNumber = intUserAnswer;
+        }
+        else 
+        {
+            Console.WriteLine($"答錯了，請輸入  {intUserAnswer} ~ {MaxNumber} 之間的數值");
+            MinNumber = intUserAnswer;
+        }
+
+    }
+} 
+
+while (answer != intUserAnswer);
+
+Console.ReadLine();
+
