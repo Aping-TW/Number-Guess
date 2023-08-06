dd16572d0da1eb143401417cf08c9930839f917c
秉諺 李 <zxcv0129@gmail.com>
2023-08-06 16:42:42 +0800

    修正，迴圈中新增continue

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 var answer = 55;
 
-int intUserAnswer;
+int intUserAnswer = 0;
 int MinNumber = 0;
 int MaxNumber = 100;
 
@@ -12,55 +12,45 @@ do
     Console.WriteLine($"請輸入介於{MinNumber}~{MaxNumber}之間的終極密碼");
     
     var userAnswer = Console.ReadLine();
-
-
+    
     if (int.TryParse(userAnswer,out int intConvertCheck))
-     {
+    {
          intConvertCheck = userAnswer.ToInt();
-        do
-        {
-            intUserAnswer = intConvertCheck;
-
-            if (answer == intUserAnswer)
-            {
-                Console.WriteLine("答對了");                
-            }
-
-            else if (intUserAnswer > MaxNumber || intUserAnswer < MinNumber)
-            {
-                Console.WriteLine("輸入的值超出範圍，請重新輸入");
-            }
-            else
-            {
-                if (intUserAnswer > answer)
-                {
-                    Console.WriteLine($"答錯了，請輸入 {MinNumber} ~ {intUserAnswer} 之間的數值");
-
-                    MaxNumber = intUserAnswer;
-                }
-                else
-                {
-                    Console.WriteLine($"答錯了，請輸入  {intUserAnswer} ~ {MaxNumber} 之間的數值");
-                    MinNumber = intUserAnswer;
-                }
-
-            }
-            break;
-        } while (answer != intUserAnswer);
-
-        
+    }
+    else
+    {
+        Console.WriteLine("輸入的值不是數字，請重新輸入。");
+        continue;
+    }
+    
+    intUserAnswer = intConvertCheck;
+    
+    if (answer == intUserAnswer)
+    {
+        Console.WriteLine("答對了");
     }
 
-     else
-     {
-         Console.WriteLine("輸入的值不是數字，請重新輸入。");
-     }
+    else if (intUserAnswer > MaxNumber || intUserAnswer < MinNumber)
+    {
+        Console.WriteLine("輸入的值超出範圍，請重新輸入");
+    }
+    else
+    {
+        if (intUserAnswer > answer)
+        {
+            Console.WriteLine($"答錯了，請輸入 {MinNumber} ~ {intUserAnswer} 之間的數值");
 
-    
-    
-} 
+            MaxNumber = intUserAnswer;
+        }
+        else
+        {
+            Console.WriteLine($"答錯了，請輸入  {intUserAnswer} ~ {MaxNumber} 之間的數值");
+            MinNumber = intUserAnswer;
+        }
 
-while (true);
+    }
+
+} while (answer != intUserAnswer);
 
 
 
